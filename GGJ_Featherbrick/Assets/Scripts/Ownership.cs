8377ad2dec77feadb0ac5c450edc4fa61b02a92e
Daniel Paul <1700099@uad.ac.uk>
2019-11-05 23:25:19 +0000

    updated trash/points when it wants to

---
+++ GGJ_Featherbrick/Assets/Scripts/Ownership.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ownership : MonoBehaviour
+{
+    public int ownerID = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ownerID = collision.gameObject.GetComponent<CharacterController>().PlayerID;
+            Debug.Log("I am owned by Player " + collision.gameObject.GetComponent<CharacterController>().PlayerID);
+        }
+    }
+}
