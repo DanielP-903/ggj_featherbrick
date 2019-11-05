8c60c2b81a1c7c06a5e0208823aa25b32da76552
H <h>
2019-11-05 14:06:54 +0000

    movement

--- GGJ_Featherbrick/Assets/Scripts/Movement.cs
+++ GGJ_Featherbrick/Assets/Scripts/Movement.cs
@@ -6,7 +6,7 @@ public class Movement : MonoBehaviour
 {
 
     public float speed = 2;
-
+    public bool is_grounded;
     Vector2 movement_position;
 
     private CharacterController characterController;
@@ -19,17 +19,46 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+        Vector3 jumpforce = new Vector3();
 
-       
+        // movement = movement.normalized * speed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") == true && is_grounded == true)
+        {
+            jumpforce = Vector3.up;
+            //float jumping = 100.0f * Time.deltaTime;
+            //this.GetComponent<Rigidbody>().MovePosition(new Vector3(transform.position.x, transform.position.y + jumping, 0));
+            is_grounded = false;
+        }
+        else
+        {
+            jumpforce = Vector3.zero;
+        }
+
+        //this.GetComponent<Rigidbody>().MovePosition(new Vector3(transform.position.x + movement.x, transform.position.y, 0));
+        this.GetComponent<Rigidbody>().AddForce(((movement * 0.75f)) + (jumpforce * 5.0f), ForceMode.Impulse);
+
 
     }
 
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+    }
 
-        movement = movement.normalized * speed * Time.deltaTime;
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Building")
+        {
+            is_grounded = true;
+        }
 
-        this.GetComponent<Rigidbody>().MovePosition(new Vector3(transform.position.x + movement.x, 0, 0));
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Building")
+        {
+            is_grounded = false;
+        }
     }
 }
