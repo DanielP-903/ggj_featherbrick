1b206b45c38cb31fef6341361ad53c55205c1c04
H <h>
2019-11-05 12:01:57 +0000

    Player movement along the x-axis

--- GGJ_Featherbrick/Assets/Scripts/Movement.cs
+++ GGJ_Featherbrick/Assets/Scripts/Movement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    public float speed = 2;
+
+    Vector2 movement_position;
+
+    private CharacterController characterController;
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+       
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        this.GetComponent<Rigidbody>().MovePosition(new Vector3(transform.position.x + movement.x, 0, 0));
     }
 }
