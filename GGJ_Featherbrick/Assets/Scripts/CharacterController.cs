7a963e3107e0bb64d7088249723d2d1edd9b9a74
1601797 <1601797@uad.ac.uk>
2019-11-05 17:20:37 +0000

    Multiplayer character controller

--- GGJ_Featherbrick/Assets/Scripts/CharacterController.cs
+++ GGJ_Featherbrick/Assets/Scripts/CharacterController.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     //player ID to track which player is controlling this character
-    public int playerID = 0;
+    public int PlayerID = 0;
 
     //Speed that the player moves at
-    public float movementSpeed = 2.0f;
+    public float MovementSpeed = 2.0f;
     //The force applied to the player jump
-    public float jumpForce = 5.0f;
+    public float JumpForce = 5.0f;
     //Check for if the player is touching the ground
-    public bool isGrounded = false;
+    public bool IsGrounded = false;
     //Movement direction indicated by the left analogue stick
-    Vector2 MovementDirection = new Vector2();
+    Vector3 MovementDirection = new Vector2();
     //Aim direction indicated by the right analogue stick
-    Vector2 AimDirection = new Vector2();
+    Vector3 AimDirection = new Vector2();
+    //The force applied to the trash when thrown
+    public float ThrowForce = 10.0f;
+    //Reference to the picked up piece of trash
+    public GameObject Trash;
 
     // Start is called before the first frame update
     void Start()
@@ -28,28 +32,28 @@ public class CharacterController : MonoBehaviour
     {
 
         //Set the movement direction along the x-axis using the left analogue stick 
-        MovementDirection = new Vector2(Input.GetAxisRaw("Player" + playerID + "LH"), 0.0f);
+        MovementDirection = new Vector3(Input.GetAxisRaw("Player" + PlayerID + "LH"), 0.0f, 0.0f);
         //Set the aim direction using the right analogue stick
-        AimDirection = new Vector2(Input.GetAxisRaw("Player" + playerID + "RH"), Input.GetAxisRaw("Player" + playerID + "RV"));
+        AimDirection = new Vector3(Input.GetAxisRaw("Player" + PlayerID + "RH"), Input.GetAxisRaw("Player" + PlayerID + "RV"), 0.0f);
         
         //If the left trigger is pressed pickup the trash
-        if (Input.GetAxisRaw("Player" + playerID + "LT") != 0)
+        if (Input.GetAxisRaw("Player" + PlayerID + "LT") != 0)
         {
-            Debug.Log("Player " + playerID + " pressed Left Trigger");
+            Debug.Log("Player " + PlayerID + " pressed Left Trigger");
             PickupTrash();
         }
 
         //If the right trigger is pressed throw the trash
-        if (Input.GetAxisRaw("Player" + playerID + "RT") != 0)
+        if (Input.GetAxisRaw("Player" + PlayerID + "RT") != 0)
         {
-            Debug.Log("Player " + playerID + " pressed Right Trigger");
+            Debug.Log("Player " + PlayerID + " pressed Right Trigger");
             ThrowTrash();
         }
 
         //If the A button is pressed then jump
-        if (Input.GetAxisRaw("Player" + playerID + "A") != 0)
+        if (Input.GetAxisRaw("Player" + PlayerID + "A") != 0)
         {
-            Debug.Log("Player " + playerID + " pressed A");
+            Debug.Log("Player " + PlayerID + " pressed A");
             Jump();
         }
     }
@@ -57,12 +61,16 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HandleInput();
+        GetComponent<Rigidbody>().AddForce(MovementDirection * MovementSpeed, ForceMode.VelocityChange);
     }
 
     void Jump()
     {
-
+        if(IsGrounded)
+        {
+            GetComponent<Rigidbody>().AddForce(JumpForce * Vector3.up, ForceMode.Impulse);
+        }
     }
 
     void PickupTrash()
@@ -72,11 +80,27 @@ public class CharacterController : MonoBehaviour
 
     void ThrowTrash()
     {
-
+        //trash.addforce(AimDirection * throwForce);
     }
 
     void ActivateShield()
     {
 
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Building")
+        {
+            IsGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "Building")
+        {
+            IsGrounded = false;
+        }
     }
 }
