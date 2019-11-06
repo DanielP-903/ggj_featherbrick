2e971e5ff2119af383d1f54db63c3b0f25c3fd52
Tomas Hudak <hudakt05@hotmail.com>
2019-11-06 11:02:49 +0000

    Added and linked menus

---
+++ GGJ_Featherbrick/Assets/Scripts/MainMenuSceneChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuSceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void Exit()
+    {
+        Application.Quit();
+    }
+
+    void GoToPlayerSelect()
+    {
+        SceneManager.LoadScene("Assets/Scenes/Menu Player Select.unity", LoadSceneMode.Single);
+    }
+}
